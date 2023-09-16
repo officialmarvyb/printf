e5e976772f9c9a24dea25cca5496ebeb3efed825
Subedat Adeyemi <subedatade@gmail.com>
2023-09-16 08:53:40 -0700

    0-printf_task.c

--- 0-printf_task.c
+++ 0-printf_task.c
@@ -45,35 +45,3 @@ int _printf(const char *format, ...)
 	va_end(args);
 	return (k);
 }
-#include "main.h"
-
-/**
- * my_printf - a simplified printf-like function for %d and %i specifiers
- * @format: The format string
- * @...: A variable number of integer arguments
- */
-void my_printf(const char *format, ...)
-{
-	va_list args;
-
-	va_start(args, format);
-
-	while (*format)
-	{
-
-		if (*format == '%' && (*(format + 1) == 'd' || *(format + 1) == 'i'))
-		{
-			int value = va_arg(args, int);
-
-			_putchar("%d", value);
-			format++;
-		}
-		else
-		{
-			_putchar(*format);
-		}
-		format++;
-	}
-
-	va_end(args);
-}

771a29de2dab62f3ec25039784e727ac2d2798dc
officialmarvyb <marvellousadeyemi5@gmail.com>
2023-09-17 13:19:33 -0700

    prototype update to header file

--- main.h
+++ main.h
@@ -6,8 +6,32 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
-int _putchar(char c);
+#define BUFFER_SIZE 1024
+
+/**
+ * struct formatspecifier - A new type describing formatspecifier
+ *
+ * @formatspecifier: The format.
+ * @fun: The function associated.
+ */
+
+struct formatspecifier
+{
+	char formatspecifier;
+	int (*fun)(va_list, char[], int, int, int, int);
+};
+
+/**
+ * typedef struct formatspecifier fmt_s - A new type describing a format.
+ *
+ * @formatspecifier: The format.
+ * @fmt_s: Typedef for struct formatspecifier
+ */
+
+typedef struct formatspecifier fmt_s;
+
 int _printf(const char *format, ...);
-void my_printf(const char *format, ...);
+int handle_print(const char *formatspecifier, int *j,
+va_list list, char buffer[], int flags, int width, int precision, int size);
 
 #endif

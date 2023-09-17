00b54c580732592c136f4eb2baed3a43042986f7
officialmarvyb <marvellousadeyemi5@gmail.com>
2023-09-17 12:55:34 -0700

    _printf flie to produces output according to specified format

--- _printf.c
+++ _printf.c
@@ -1,47 +1,64 @@
 #include "main.h"
+
+void print_buffer(char buffer[], int *buffer_index);
+
 /**
- * _printf - Produces output according to a format.
+ * _printf - produce output according to a format.
  * @format: A character string containing format specifiers.
  *
  * Return: The number of characters printed (excluding the null byte).
  */
+
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int k = 0;
-	char *str;
-	/*Initialize the va_list with the variable number of arguments*/
-	va_start(args, format);
-	while (format && *format)
+	int j, c = 0, chars = 0;
+	int flags, width, precision, size, buffer_index = 0;
+	va_list arg;
+	char buffer[BUFFER_SIZE];
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(arg, format);
+
+	for (j = 0; format && format[j] != '\0'; j++)
 	{
-		if (*format == '%' && format++)
+		if (format[j] != '%')
 		{
-			if (*format == 'c')
-				k += _putchar(va_arg(args, int));
-			else if (*format == 's')
-			{
-				str = va_arg(args, char *);
-				if (str == NULL)
-					str = "(null)";
-				while (*str)
-					k += _putchar(*str++);
-			}
-			else if (*format == 'd')
-			{
-				k += _putchar(va_arg(args, int));
-			}
-			else if (*format == '%')
-				k += _putchar('%');
-			else
-			{
-				k += _putchar('%');
-				k += _putchar(*format);
-			}
+			buffer[buffer_index++] = format[j];
+			if (buffer_index == BUFFER_SIZE)
+				print_buffer(buffer, &buffer_index);
+			chars++;
 		}
 		else
-			k += _putchar(*format);
-		format++;
+		{
+			print_buffer(buffer, &buffer_index);
+			flags = get_flags(format, &j);
+			width = get_width(format, &j, arg);
+			precision = get_precision(format, &j, arg);
+			size = get_size(format, &j);
+			++j;
+			c = handle_print(format, &j, arg, buffer,
+					flags, width, precision, size);
+			if (c == -1)
+				return (-1);
+			chars += c;
+		}
 	}
-	va_end(args);
-	return (k);
+	print_buffer(buffer, &buffer_index);
+	va_end(arg);
+	return (chars);
+}
+
+/**
+ * print_buffer - Prints the contents of the buffer if encountered
+ * @buffer: Array of characters
+ * @buffer_index: Index to add next character
+ */
+
+void print_buffer(char buffer[], int *buffer_index)
+{
+	if (*buffer_index > 0)
+		write(1, &buffer[0], *buffer_index);
+	*buffer_index = 0;
 }
